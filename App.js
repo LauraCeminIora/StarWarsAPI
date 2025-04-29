9d5bd8bfd32b54e5d270da79543ec291680cc288
Júlia Wonsick <1136562@atitus.edu.br>
2025-04-29 19:32:24 -0300

    Adição da reprodução automática da música de Star Wars

--- App.js
+++ App.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from 'react-native'; 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; 
-import CharactersScreen from './screens/CharactersScreen';
-import CharacterDetailsScreen from './screens/CharacterDetailsScreen';
-import ShipsScreen from './screens/ShipsScreen';
-import FilmsScreen from './screens/FilmsScreen';
-import AboutScreen from './screens/AboutScreen';
+import { Audio } from 'expo-av'; 
+import CharactersScreen from './screens/CharactersScreen.js';
+import CharacterDetailsScreen from './screens/CharacterDetailsScreen.js';
+import ShipsScreen from './screens/ShipsScreen.js';
+import FilmsScreen from './screens/FilmsScreen.js';
+import AboutScreen from './screens/AboutScreen.js';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [sound, setSound] = useState();
+
+  useEffect(() => {
+    const playSound = async () => {
+      try {
+        // Permissões e configurações para garantir reprodução
+        await Audio.requestPermissionsAsync(); // Para iOS
+        await Audio.setAudioModeAsync({
+          playsInSilentModeIOS: true,
+        });
+
+        const { sound } = await Audio.Sound.createAsync(
+          require('./assets/MusicaStarWars.mp3')
+        );
+        setSound(sound);
+        await sound.playAsync(); 
+      } catch (error) {
+        console.error("Erro ao carregar o áudio:", error);
+      }
+    };
+
+    playSound();
+
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Personagens" component={CharactersScreen} options={({ navigation }) => ({
+      <Stack.Navigator
+        screenOptions={({ navigation }) => ({
           headerRight: () => (
-            <Text onPress={() => navigation.navigate('Sobre')} style={{ marginRight: 10 }}>Sobre</Text>
+            <Text onPress={() => navigation.navigate('Sobre')} style={{ marginRight: 10 }}>
+              Sobre
+            </Text>
           )
-        })} />
+        })}
+      >
+        <Stack.Screen 
+          name="Personagens" 
+          component={CharactersScreen} 
+        />
         <Stack.Screen name="Details" component={CharacterDetailsScreen} options={{ title: 'Detalhes' }} />
         <Stack.Screen name="Ships" component={ShipsScreen} options={{ title: 'Naves' }} />
         <Stack.Screen name="Films" component={FilmsScreen} options={{ title: 'Filmes' }} />
